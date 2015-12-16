5d983866eb4bd973818297f2e944c1a9fab3dbb6
Yuriy Kryshchuk <yuriy.kryshchuk@gmail.com>
2015-12-16 16:43:39 +0200

    Javadoc fixes

--- src/main/java/com/kryshchuk/maven/plugins/fixsource/AbstractFixMojo.java
+++ src/main/java/com/kryshchuk/maven/plugins/fixsource/AbstractFixMojo.java
@@ -1,17 +1,17 @@
 /*
  * FixSource Maven Plugin
  * Copyright (C) 2013  Yuriy Kryshchuk
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses />.
  */
@@ -71,6 +71,7 @@ public abstract class AbstractFixMojo extends AbstractMojo {
    * @param header
    *          the header to set
    * @throws IOException
+   *           if could not read from file
    */
   public void setHeader(final String header) throws IOException {
     this.header = header;
@@ -81,6 +82,7 @@ public abstract class AbstractFixMojo extends AbstractMojo {
    * @param headerFile
    *          the headerFile to set
    * @throws IOException
+   *           if could not read file
    */
   public void setHeaderFile(final File headerFile) throws IOException {
     this.headerFile = headerFile;
@@ -96,6 +98,7 @@ public abstract class AbstractFixMojo extends AbstractMojo {
 
   /**
    * @since 1.1.5
+   * @return true if should fix header
    */
   protected boolean isFixHeader() {
     return fixHeader;
@@ -119,7 +122,7 @@ public abstract class AbstractFixMojo extends AbstractMojo {
   protected abstract AbstractSource createSource();
 
   /**
-   * @return
+   * @return set of source locations
    */
   protected abstract FileSet getSourcesSet();
 
